51ce6040eaa8e2376a529c464b2846378e7ac811
ZippyPixel <kabbo1997@gmail.com>
2022-11-01 16:24:14 +0600

    heap sort added

--- Sort.java
+++ Sort.java
@@ -8,7 +8,7 @@ public class Sort {
         }
 //        int[] arr = {9,8,7,6,5,4,3,1,2};
         long start = System.currentTimeMillis();
-        quick(arr);
+        merge(arr);
         for (int i : arr){
             System.out.print(i+" ");
         }
@@ -186,6 +186,48 @@ public class Sort {
         }
         for (int i=0; i< merged.length; i++){
             arr[low+i] = merged[i];
+        }
+    }
+    public static void heap(int arr[]){
+        int N = arr.length;
+
+        // Build heap (rearrange array)
+        for (int i = N / 2 - 1; i >= 0; i--)
+            heapify(arr, N, i);
+
+        // One by one extract an element from heap
+        for (int i = N - 1; i > 0; i--) {
+            // Move current root to end
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            // call max heapify on the reduced heap
+            heapify(arr, i, 0);
+        }
+    }
+    public static void heapify(int arr[], int N, int i)
+    {
+        int largest = i; // Initialize largest as root
+        int l = 2 * i + 1; // left = 2*i + 1
+        int r = 2 * i + 2; // right = 2*i + 2
+
+        // If left child is larger than root
+        if (l < N && arr[l] > arr[largest])
+            largest = l;
+
+        // If right child is larger than largest so far
+        if (r < N && arr[r] > arr[largest])
+            largest = r;
+
+        // If largest is not root
+        if (largest != i) {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+
+            // Recursively heapify the affected sub-tree
+            heapify(arr, N, largest);
         }
     }
 }
